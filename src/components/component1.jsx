f21b24e3d8c2ccaf5be2680240953241bb33385a
Gelmini Juri <nicolas.gelmini.juri@accenture.com>
2019-11-05 11:29:35 -0300

    first commit

---
+++ src/components/component1.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class component1 extends Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    
+                Hola amigos! {this.props}
+                </p>
+                <button>
+
+                </button>
+            </div>
+        )
+    }
+}
---
+++ src/containers/container1.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Component1 from '../components/component1'
+
+function cambiarColor(){
+        
+}
+
+export default class container1 extends Component {
+    render() {
+        return (
+            
+                <Component1 buscarUsuario = {buscarUsuario}/>
+            
+        )
+    }
+}
